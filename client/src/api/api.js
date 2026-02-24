a28a730dd9e370b8ab1cd49789b7b557497cd0b8
Sulabh Saluja <sulabhsaluja111@gmail.com>
2026-02-24 16:11:35 +0530

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://seat-booking-system-bsa5.onrender.com"
+  baseURL: "https://seat-booking-system-bsa5.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
