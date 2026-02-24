78157fa246aad3a8e1346115283c65754b48fefe
Sulabh Saluja <sulabhsaluja111@gmail.com>
2026-02-24 16:01:12 +0530

    Update server.js
    Removed CORS configuration from the server.

--- server/server.js
+++ server/server.js
@@ -2,13 +2,6 @@ require("dotenv").config();
 const connectDB = require("./src/config/db");
 const app = require("./app");
 
-const cors = require("cors");
-
-app.use(cors({
-  origin: "https://seating-arrangement-tlkw.onrender.com/",
-  credentials: true
-}));
-
 connectDB();
 
 const PORT = process.env.PORT || 5000;
